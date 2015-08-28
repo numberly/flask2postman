fc4f7cbcb0b2d90d65e278f7d1a5b433e1396882
Guillaume Gelin <ramnes@1000mercis.com>
2015-08-28 18:27:36 +0200

    Release 1.0.3

--- flask2postman.py
+++ flask2postman.py
@@ -6,7 +6,7 @@ from copy import copy
 from time import time
 from uuid import uuid4
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 methods_order = ["GET", "POST", "PUT", "PATCH", "DELETE", "COPY", "HEAD",
                  "OPTIONS", "LINK", "UNLINK", "PURGE"]
